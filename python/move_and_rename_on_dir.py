b087c48e2f5fc9bab58c824db1cd377ccc4d9808
Josh Ziegler <josh.s.ziegler@gmail.com>
2016-04-16 14:05:16 -0400

    Updated copyright notices

--- python/move_and_rename_on_dir.py
+++ python/move_and_rename_on_dir.py
@@ -1,7 +1,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2014 Joshua S. Ziegler 
+Copyright (c) 2014-2016 Joshua S. Ziegler 
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
