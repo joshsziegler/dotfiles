247bb6152e10d34235407bac8fbeecf161ff00f2
Josh Ziegler <josh.s.ziegler@gmail.com>
2016-05-02 20:56:43 -0400

    Broke feeds.py out of rss_to_email.py

---
+++ python/feeds.py
@@ -0,0 +1,130 @@
+#! /usr/bin/python
+
+"""Parses RSS/Atom feeds and returns them as HTML.
+
+
+The MIT License (MIT)
+
+Copyright (c) 2014-2016 Joshua S. Ziegler 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import datetime, traceback, argparse
+import feedparser
+
+feeds = [ # These will be printed in-order, so put important stuff first
+    # Comics
+    ("http://penny-arcade.com/feed", "Penny Arcade"),
+    ("http://feeds.feedburner.com/thedoghousediaries/feed", "Doghouse Diaries "),
+    ("http://www.smbc-comics.com/rss.php", "SMBC"),
+    ("http://theoatmeal.com/feed/rss", "The Oatmeal"),
+    ("http://xkcd.com/rss.xml", "XKCD"),
+    # News, blogs, etc.
+    ("http://www.vox.com/authors/matthew-yglesias/rss", "Vox: Matt Yglesias"),
+    ("http://feeds.arstechnica.com/arstechnica/features", "Ars Technica"),
+    ("http://www.wired.com/threatlevel/feed/", "Wired: Threat Level"),
+    ("http://feeds.wired.com/WiredDangerRoom", "Wired: Danger Room"),
+    ("http://www.vox.com/rss/index.xml", "Vox"),
+    ("https://news.ycombinator.com/rss", "Hacker News")
+]
+
+plain_links = [ # Simply put as-is in the email 
+    #("http://hckrnews.com/", "Hckr News")
+]
+
+
+def get_feeds(days, past_links_path):
+    today = datetime.date.today()
+    x_days_ago = today - datetime.timedelta(days=days)
+    x_days_ago = x_days_ago.timetuple() # feedparser uses timetuples
+
+    past_links = []
+    try:
+        past_links = open(past_links_path, "r+").read().split('\n') 
+    except:
+        pass # no file found
+
+    title = "RSS Feeds for {}".format(today.isoformat())
+    plain = "{}\n".format(title)
+    html =  "<h1>{}</h1>".format(title)
+    for feed, feed_title in feeds:
+        try:
+            posts = feedparser.parse(feed)
+            if len(posts['entries']) < 1: # skip feeds without any stories
+                continue
+
+            plain += "{}:\n".format(feed_title)
+            html  += "<h3>{}</h3>".format(feed_title)
+            for post in posts['entries']:
+                title = post['title']
+                link  = post['link']
+                new   = True
+
+                if link in past_links:
+                    continue # skip this old link
+
+                try: 
+                    if post['published_parsed'] < x_days_ago:
+                        continue # skip this old link
+                except:
+                    pass 
+
+                try: # some feeds don't seem to have published as a key
+                    if post['updated_parsed'] < x_days_ago:
+                        continue # skip this old link
+                except:
+                    pass 
+
+                # include it by default if not in past_link file or published date
+                past_links.append(link)
+                plain += "    {} {}\n".format(title, link)
+                html  += "&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"{}\">{}</a><br>".format(link, title)
+
+        except:
+            err = "Choked on {}\n{}\n\n".format(feed, traceback.format_exc())
+            plain += err
+            html  += err
+
+    plain += "\n"
+    html += "<br>"
+
+    for link, pretty_name in plain_links:
+        plain += "{} {}\n".format(title, link)
+        html  += "<a href=\"{}\">{}</a><br>".format(link, pretty_name)
+
+    html += "</body></html>"
+
+    open(past_links_path, "w+").write('\n'.join(past_links))
+
+    return (title, plain, html)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--html', action='store_true')
+    parser.add_argument('--past_links', type=str, default="past_links.txt")
+    parser.add_argument('--days',       type=int, default=1)
+    args = parser.parse_args()
+
+    title, plain, html = get_feeds(args.days, args.past_links)
+    html = "<html><body>{}</body></html>".format(html)
+    if args.html:
+        print(html.encode('ascii', 'ignore'))
+    else:
+        print(plain.encode('ascii', 'ignore'))
--- python/rss_to_email.py
+++ python/rss_to_email.py
@@ -26,28 +26,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-import datetime, traceback, smtplib, argparse, email.message
-import feedparser
-
-feeds = [ # These will be printed in-order, so put important stuff first
-    # Comics
-    ("http://penny-arcade.com/feed", "Penny Arcade"),
-    ("http://feeds.feedburner.com/thedoghousediaries/feed", "Doghouse Diaries "),
-    ("http://www.smbc-comics.com/rss.php", "SMBC"),
-    ("http://theoatmeal.com/feed/rss", "The Oatmeal"),
-    ("http://xkcd.com/rss.xml", "XKCD"),
-    # News, blogs, etc.
-    ("http://www.vox.com/authors/matthew-yglesias/rss", "Vox: Matt Yglesias"),
-    ("http://feeds.arstechnica.com/arstechnica/features", "Ars Technica"),
-    ("http://www.wired.com/threatlevel/feed/", "Wired: Threat Level"),
-    ("http://feeds.wired.com/WiredDangerRoom", "Wired: Danger Room"),
-    ("http://www.vox.com/rss/index.xml", "Vox"),
-    ("https://news.ycombinator.com/rss", "Hacker News")
-]
-
-plain_links = [ # Simply put as-is in the email 
-    #("http://hckrnews.com/", "Hckr News")
-]
+import smtplib, argparse, email.message
 
 
 def send_email(gmail_user, gmail_pwd, to, subject, text, html):
@@ -77,64 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('--days',      type=int, default=1)
     args = parser.parse_args()
 
-    today = datetime.date.today()
-    x_days_ago = today - datetime.timedelta(days=args.days)
-    x_days_ago = x_days_ago.timetuple() # feedparser uses timetuples
+    title, plain, html = get_feeds(args.days, args.past_links)
+    html = "<html><body>{}</body></html>".format(html)
 
-    past_links = []
-    try:
-        past_links = open(args.past_links, "r+").read().split('\n') 
-    except:
-        pass # no file found
-
-    plain = ""
-    html = "<html><head></head><body>"
-    for feed, feed_title in feeds:
-        try:
-            posts = feedparser.parse(feed)
-            if len(posts['entries']) < 1: # skip feeds without any stories
-                continue
-
-            plain += "{}:\n".format(feed_title)
-            html  += "<b><a href=\"{}\" style=\"text-decoration: none;color: black;\">{}</a></b>:<br>".format(feed, feed_title)
-            for post in posts['entries']:
-                title = post['title']
-                link  = post['link']
-                new   = True
-
-                if link in past_links:
-                    continue # skip this old link
-
-                try: 
-                    if post['published_parsed'] < x_days_ago:
-                        continue # skip this old link
-                except:
-                    pass 
-
-                try: # some feeds don't seem to have published as a key
-                    if post['updated_parsed'] < x_days_ago:
-                        continue # skip this old link
-                except:
-                    pass 
-
-                # include it by default if not in past_link file or published date
-                past_links.append(link)
-                plain += "    {} {}\n".format(title, link)
-                html  += "&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"{}\" style=\"text-decoration: none;color: #444;\">{}</a><br>".format(link, title)
-
-        except:
-            err = "Choked on {}\n{}\n\n".format(feed, traceback.format_exc())
-            plain += err
-            html  += err
-
-    plain += "\n"
-    html += "<br>"
-
-    for link, pretty_name in plain_links:
-        plain += "{} {}\n".format(title, link)
-        html  += "<a href=\"{}\" style=\"text-decoration: none;color: #444;\">{}</a><br>".format(link, pretty_name)
-
-    html += "</body></html>"
-    send_email(args.user, args.password, args.to, "RSS Feeds for {}".format(today.isoformat()), plain, html)
-
-    open(args.past_links, "w+").write('\n'.join(past_links))
+    send_email(args.user, args.password, args.to, title plain, html)
