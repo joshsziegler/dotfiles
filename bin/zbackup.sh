5df9a176c6b8b4a0844b7b010c9d8b9d432bdfb9
Josh Ziegler <josh.s.ziegler@gmail.com>
2019-10-31 20:50:18 -0400

    Update zbackup's documentation and TODOs

--- bin/zbackup.sh
+++ bin/zbackup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash                                                                                              
+
+# A simple rsync over SSH backup script for multiple hosts.
+# 
+# - Stores a single full daily, weekly, and monthly backup in /srv/backups/
+#   - Example: If host is foo then daily would be in /srv/backups/foo/daily/
+# - Handles multiple hosts with per-host usernames over SSH
+# - Stores a "backed up on" text file in the directory to make identification easier
+#
                                                                                                          
 BACKUP_DIR="/srv/backups"                                                                                
 DIRS="{app,etc,home,opt,root,srv,usr/local,var/log,var/spool,var/www}"                                   
@@ -17,6 +25,8 @@ for UserHost in ubuntu@srv-1 ubunt@srv-2 ubuntu@srv-3 ubuntu@code ubuntu@vm-fati
     MDIR="$BDIR/monthly" # Monthly Backup Directory                                                      
                                                                                                          
     echo "*** Creating daily backup for $HOST on $DATE..."                                               
+    # TODO: Re-enable --ignore-missing-args to avoid non-errors for missing 
+    #       directories (doesn't work with old versions of rsync)
     #rsync -aq --delete --numeric-ids --ignore-missing-args -e "ssh" --rsync-path="sudo rsync" $USER@$HOST:/$DIRS $BDIR                                                                                           
     rsync -aq --delete --numeric-ids -e "ssh" --rsync-path="sudo rsync" $USER@$HOST:/$DIRS $DDIR         
     # Create a file in the backup indicating when this back was created for easy identification          
@@ -30,6 +40,7 @@ for UserHost in ubuntu@srv-1 ubunt@srv-2 ubuntu@srv-3 ubuntu@code ubuntu@vm-fati
     fi                                                                                                   
                                                                                                          
     # Run monthly backup on the first day of the month (sync currently weekly to the monthly directory)  
+    # TODO: Only run if the daily succeeds to avoid overwriting good data?                               
     if [ "$DOM" == "1" ]; then                                                                           
         echo "*** Creating monthly backup..."                                                            
         rsync -aq --delete --numeric-ids $WDIR $MDIR                                                     
