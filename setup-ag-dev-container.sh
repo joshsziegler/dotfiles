1b4ddcc689dca0afe6806423d9c6a8f962fb1e14
Josh Ziegler <josh.s.ziegler@gmail.com>
2021-12-09 09:29:34 -0500

    Add script for ag-dev container setup

---
+++ setup-ag-dev-container.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e # Stop execution if a command errors
+set -x # Echo commands and expand any variables
+
+# Update repos
+sudo apt update
+# Install packages
+sudo apt install -y vim 
+# Setup .ssh/config for office git repo
+cat << EOF >> /home/vscode/.ssh/config
+# Dev or Remote .ssh/config
+Host office.infinitetactics.com
+    User joshz
+Host gitlab.infinitetactics.com
+    HostName gitlab    
+    ProxyJump office.infinitetactics.com
+    User git
+EOF
+# Fix ownership of Go binaries and directories
+sudo chown vscode:vscode -R /usr/local/go
+sudo chown vscode:vscode -R /go
+# Setup bash aliases IFF they aren't in the file already
+grep -qxF 'alias gitf="git fetch --all --prune"' ~/.bashrc || echo 'alias gitf="git fetch --all --prune"' >> ~/.bashrc
+# Don't forget to run 'make develop' in repo 
