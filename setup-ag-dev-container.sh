ff1c46f739055260ddc786d4882bc8eb3f3a4fef
Josh Ziegler <josh.s.ziegler@gmail.com>
2021-12-15 17:40:21 -0500

    Add alternative IT gitlab to SSH config

--- setup-ag-dev-container.sh
+++ setup-ag-dev-container.sh
@@ -21,4 +21,5 @@ sudo chown vscode:vscode -R /usr/local/go
 sudo chown vscode:vscode -R /go
 # Setup bash aliases IFF they aren't in the file already
 grep -qxF 'alias gitf="git fetch --all --prune"' ~/.bashrc || echo 'alias gitf="git fetch --all --prune"' >> ~/.bashrc
+grep -qxF 'alias gitl="git log --all --oneline --graph"' ~/.bashrc || echo 'alias gitl="git log --all --oneline --graph"' >> ~/.bashrc
 # Don't forget to run 'make develop' in repo 
