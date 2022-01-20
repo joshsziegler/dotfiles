25e8a13c7791171f34e68c5c79ab57b69a74868f
Josh Ziegler <josh.s.ziegler@gmail.com>
2022-01-20 13:09:17 -0500

    Add step to install Task

--- setup-ag-dev-container.sh
+++ setup-ag-dev-container.sh
@@ -21,4 +21,6 @@ sudo chown vscode:vscode -R /go
 # Setup bash aliases IFF they aren't in the file already
 grep -qxF 'alias gitf="git fetch --all --prune"' ~/.bashrc || echo 'alias gitf="git fetch --all --prune"' >> ~/.bashrc
 grep -qxF 'alias gitl="git log --all --oneline --graph"' ~/.bashrc || echo 'alias gitl="git log --all --oneline --graph"' >> ~/.bashrc
+# Install Task (Make replacement)
+sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
 # Don't forget to run 'make develop' in repo 
