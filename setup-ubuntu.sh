527b3e92c97d8070488ccca8fd83d5206774e8a2
Josh Ziegler <josh.s.ziegler@gmail.com>
2023-04-05 09:33:04 -0400

    Use agadmin user on ag-dev; Optionally install SublimeText on Ubuntu

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -120,6 +120,16 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     rm vscode.deb
 fi
 
+# Sublime Text
+read -p "Install Sublime Text (Y or N)? " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt update
+    sudo apt install sublime-text
+fi
+
 # Slack
 read -p "Install Slack (Y or N)? " -n 1 -r
 echo ""
