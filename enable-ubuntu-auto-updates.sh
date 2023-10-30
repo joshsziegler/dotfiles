2d6fc4e70d43a25618ac8f0278fdc33ef19a668d
Josh Ziegler <josh.s.ziegler@gmail.com>
2023-10-30 12:03:16 -0400

    Remove step that cats unattended-upgrades conf

--- enable-ubuntu-auto-updates.sh
+++ enable-ubuntu-auto-updates.sh
@@ -4,7 +4,6 @@
 # Adapted from https://www.digitalocean.com/community/tutorials/how-to-keep-ubuntu-20-04-servers-updated
 sudo apt-get install unattended-upgrades
 sudo systemctl status unattended-upgrades.service
-sudo cat /etc/apt/apt.conf.d/50unattended-upgrades
 sudo sed -i 's/\/\/"${distro_id}:${distro_codename}-security";/"${distro_id}:${distro_codename}-security";/g' /etc/apt/apt.conf.d/50unattended-upgrades
 sudo sed -i 's/\/\/Unattended-Upgrade::Remove-Unused-Dependencies "false";/Unattended-Upgrade::Remove-Unused-Dependencies "true";/g' /etc/apt/apt.conf.d/50unattended-upgrades
 sudo sed -i 's/\/\/Unattended-Upgrade::Automatic-Reboot "false";/Unattended-Upgrade::Automatic-Reboot "true";/g' /etc/apt/apt.conf.d/50unattended-upgrades
