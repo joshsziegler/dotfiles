a88a08b0cd71546c6959f8febbccb15b5d66e051
Josh Ziegler <josh.s.ziegler@gmail.com>
2024-04-29 12:08:55 -0400

    Add Terraform to setup-ubuntu.sh

--- bin/setup-ubuntu.sh
+++ bin/setup-ubuntu.sh
@@ -159,6 +159,16 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo apt-get update && sudo apt-get install packer
 fi
 
+# HashiCorp Terraform
+read -p "Install HashiCorp Terraform (Y or N)?" -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # This method was take from hashicorp.com on 2024.04.04
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update && sudo apt-get install terraform
+fi
+
 # Docker
 read -p "Install Docker (Y or N)?" -n 1 -r
 echo ""
